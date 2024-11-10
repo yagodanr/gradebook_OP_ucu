c75ce19d810c6c837ae32cd11a09605df5c61c06
Anastasiia Yablunovska <yablunovska.pn@ucu.edu.ua>
2024-11-10 23:24:12 +0200

    add code to function read_activities_from_sylabus

--- main.py
+++ main.py
@@ -56,15 +56,18 @@ def read_grades_from_file(filepath: str) -> dict:
 
 def read_activities_from_sylabus(filepath: str) -> dict:
     """
-    reads activities types and max grades.
+    Reads activities types and max grades from a JSON file.
 
     Args:
-        filepath (str): json file
+        filepath (str): Path to the JSON file.
 
     Returns:
-        dict: activities_max
+        dict: Dictionary containing activities and their max grades.
     """
-    ...
+    with open(filepath, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        return data
+
 
 def add_activity(gradebook: dict, activities: dict, activity: tuple[str, str], max_grade: float) -> None:
     """
